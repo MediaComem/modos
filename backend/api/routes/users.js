5e1d9e89fff3510d5f11ce3395c3f695e89b5741
Quentin Vaucher <quentin.vaucher@he-arc.ch>
2019-08-26 16:49:51 +0200

    API: add POST request for users

--- backend/api/routes/users.js
+++ backend/api/routes/users.js
@@ -1,9 +1,21 @@
+var createError = require('http-errors');
 var express = require('express');
 var router = express.Router();
+const models = require('../models')
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+
+router.post('/', function(req, res, next) {
+  const user = models.sequelize.model('user');
+
+  user.create(req.body).then((user) => {
+    res.status(201).location(`v1/users/${user.id}`).json(user)
+  }).catch((error) => {
+    if (error instanceof models.Sequelize.ValidationError) {
+      next(createError(422));
+      // res.status(422)
+      // .render('error', { error: error });
+    }
+  });
 });
 
 module.exports = router;
