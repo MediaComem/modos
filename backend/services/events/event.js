50efe49e81d63ce842ec4648ff1b831d2041659e
Simon Oulevay <simon.oulevay@heig-vd.ch>
2019-12-16 09:18:42 +0100

    Add utility function to avoid repeated try/catch in route implementations

--- backend/services/events/event.js
+++ backend/services/events/event.js
@@ -1,85 +1,58 @@
 const Event = require('../../models/event');
 const User = require('../../models/user');
 const error = require('../error');
+const { createAsyncRoute } = require('../utils');
 
-const getEvents = async (req, res) => {
-    try {
-        const events = await Event.find({});
-        if (events.length > 0) return res.status(200).json(events);
-        return error.createError(res, 404, 'No events found in the system');
-    } catch (err) {
-        return error.handleError(err, res);
-    }
-};
+const getEvents = createAsyncRoute(async (req, res) => {
+    const events = await Event.find({});
+    if (events.length > 0) return res.status(200).json(events);
+    return error.createError(res, 404, 'No events found in the system');
+});
 
-const getEventById = async (req, res) => {
-    try {
-        const event = await Event.findById(req.params.id);
-        if (event) return res.status(200).json(event);
-        return error.createError(res, 404, 'Event does not exist');
-    } catch (err) {
-        return error.handleError(err, res);
-    }
-};
+const getEventById = createAsyncRoute(async (req, res) => {
+    const event = await Event.findById(req.params.id);
+    if (event) return res.status(200).json(event);
+    return error.createError(res, 404, 'Event does not exist');
+});
 
-const createEvent = async (req, res) => {
-    try {
-        const owner = await User.findById(req.body.owner);
-        if (!owner) return error.createError(res, 404, 'Event\'s owner does not exist');
+const createEvent = createAsyncRoute(async (req, res) => {
+    const owner = await User.findById(req.body.owner);
+    if (!owner) return error.createError(res, 404, 'Event\'s owner does not exist');
 
-        const newEvent = await Event.create(req.body);
-        return res.status(201)
-            .location(`api/v1/events/${newEvent._id}`)
-            .json(newEvent);
-    } catch (err) {
-        return error.handleError(err, res);
-    }
-};
+    const newEvent = await Event.create(req.body);
+    return res.status(201)
+        .location(`api/v1/events/${newEvent._id}`)
+        .json(newEvent);
+});
 
-const updateEvent = async (req, res) => {
-    try {
-        const eventId = req.params.id;
-        const updatedEvent = await Event.findByIdAndUpdate(eventId, req.body, {
-            new: true,
-            runValidators: true
-        });
-        if (updateEvent) res.status(200).json(updatedEvent);
-        return error.createError(res, 404, 'Event does not exist');
-    } catch (err) {
-        return error.handleError(err, res);
-    }
-};
+const updateEvent = createAsyncRoute(async (req, res) => {
+    const eventId = req.params.id;
+    const updatedEvent = await Event.findByIdAndUpdate(eventId, req.body, {
+        new: true,
+        runValidators: true
+    });
+    if (updateEvent) res.status(200).json(updatedEvent);
+    return error.createError(res, 404, 'Event does not exist');
+});
 
-const deleteEvent = async (req, res) => {
-    try {
-        const event = await Event.findByIdAndRemove(req.params.id);
-        if (event) return res.status(204).json({});
-        return error.createError(res, 404, 'Event does not exist');
-    } catch (err) {
-        return error.handleError(err, res);
-    }
-};
+const deleteEvent = createAsyncRoute(async (req, res) => {
+    const event = await Event.findByIdAndRemove(req.params.id);
+    if (event) return res.status(204).json({});
+    return error.createError(res, 404, 'Event does not exist');
+});
 
-const getParticipants = async (req, res) => {
-    try {
-        const participantsRaw = await User.find({ events: req.params.id }, '_id');
-        const participants = participantsRaw.map(user => user._id);
-        if (participants.length > 0) return res.status(200).json(participants);
-        return error.createError(res, 404, 'No participants to this event');
-    } catch (err) {
-        return error.handleError(err, res);
-    }
-};
+const getParticipants = createAsyncRoute(async (req, res) => {
+    const participantsRaw = await User.find({ events: req.params.id }, '_id');
+    const participants = participantsRaw.map(user => user._id);
+    if (participants.length > 0) return res.status(200).json(participants);
+    return error.createError(res, 404, 'No participants to this event');
+});
 
-const getObservations = async (req, res) => {
-    try {
-        const observations = await Event.findById(req.params.id, 'observations');
-        if (observations.length > 0) return res.status(200).json(observations);
-        return error.createError(res, 404, 'This event has no observations');
-    } catch (err) {
-        return error.handleError(err, res);
-    }
-};
+const getObservations = createAsyncRoute(async (req, res) => {
+    const observations = await Event.findById(req.params.id, 'observations');
+    if (observations.length > 0) return res.status(200).json(observations);
+    return error.createError(res, 404, 'This event has no observations');
+});
 
 module.exports = {
     getEvents: getEvents,
--- backend/services/users/user.js
+++ backend/services/users/user.js
@@ -2,114 +2,82 @@ const User = require('../../models/user');
 const Event = require('../../models/event');
 const Observation = require('../../models/observation');
 const error = require('../error');
+const { createAsyncRoute } = require('../utils');
+
+const getUsers = createAsyncRoute(async (req, res) => {
+    const users = await User.find({});
+    if (users.length > 0) return res.status(200).json(users);
+    return error.createError(res, 404, 'No users found in the system');
+});
+
+const getUserById = createAsyncRoute(async (req, res) => {
+    const user = await User.findById(req.params.id);
+    if (user) return res.status(200).json(user);
+    return error.createError(res, 404, 'User does not exist');
+});
+
+const createUser = createAsyncRoute(async (req, res) => {
+    // Make sure the user doesn't try to modify other fields (e.g. events).
+    const temp = {
+        pseudonym: req.body.pseudonym,
+        email: req.body.email,
+        password: req.body.password
+    };
+    const newUser = await User.create(temp);
+    return res.status(201)
+        .location(`api/v1/users/${newUser._id}`)
+        .json(newUser);
+});
+
+const updateUser = createAsyncRoute(async (req, res) => {
+    const userId = req.params.id;
+    const temp = {
+        pseudonym: req.body.pseudonym,
+        email: req.body.email,
+        password: req.body.password
+    };
+    const updatedUser = await User.findByIdAndUpdate(userId, temp, {
+        new: true,
+        runValidators: true
+    });
+    if (updatedUser) return res.status(200).json(updatedUser);
+    return error.createError(res, 404, 'User does not exist');
+});
 
 
-const getUsers = async (req, res) => {
-    try {
-        const users = await User.find({});
-        if (users.length > 0) return res.status(200).json(users);
-        return error.createError(res, 404, 'No users found in the system');
-    } catch (err) {
-        return error.handleError(err, res);
+const deleteUser = createAsyncRoute(async (req, res) => {
+    const user = await User.findByIdAndRemove(req.params.id);
+    console.log(user);
+    if (user) return res.status(204).json({});
+    return error.createError(res, 404, 'User does not exist');
+});
+
+const getUserEvents = createAsyncRoute(async (req, res) => {
+    const userEvents = await Event.find({ owner: req.params.id });
+    if (userEvents) return res.status(200).json(userEvents);
+    return error.createError(res, 404, 'User does not exist');
+});
+
+const getUserObservations = createAsyncRoute(async (req, res) => {
+    const observations = await Observation.find({ owner: req.params.id });
+    if (observations) return res.status(200).json(observations);
+    return error.createError(res, 404, 'User does not exist');
+});
+
+const joinEvent = createAsyncRoute(async (req, res) => {
+    const user = await User.findById(req.params.id);
+    if (!user) return createError(res, 404, 'User does not exist');
+
+    const event = await Event.findById(req.params.eventId, '_id');
+    if (!event) return createError(res, 404, 'Event does not exist');
+    if (user.events.includes(event._id)) {
+        return error.createError(res, 422, 'User already joined the event');
     }
-};
 
-const getUserById = async (req, res) => {
-    try {
-        const user = await User.findById(req.params.id);
-        if (user) return res.status(200).json(user);
-        return error.createError(res, 404, 'User does not exist');
-    } catch (err) {
-        return error.handleError(err, res);
-    }
-};
-
-const createUser = async (req, res) => {
-    try {
-        // Make sure the user doesn't try to modify other fields (e.g. events).
-        const temp = {
-            pseudonym: req.body.pseudonym,
-            email: req.body.email,
-            password: req.body.password
-        };
-        const newUser = await User.create(temp);
-        return res.status(201)
-            .location(`api/v1/users/${newUser._id}`)
-            .json(newUser);
-    } catch (err) {
-        return error.handleError(err, res);
-    }
-};
-
-const updateUser = async (req, res) => {
-    try {
-        const userId = req.params.id;
-        const temp = {
-            pseudonym: req.body.pseudonym,
-            email: req.body.email,
-            password: req.body.password
-        };
-        const updatedUser = await User.findByIdAndUpdate(userId, temp, {
-            new: true,
-            runValidators: true
-        });
-        if (updatedUser) return res.status(200).json(updatedUser);
-        return error.createError(res, 404, 'User does not exist');
-    } catch (err) {
-        error.handleError(err, res);
-    }
-};
-
-
-const deleteUser = async (req, res) => {
-    try {
-        const user = await User.findByIdAndRemove(req.params.id);
-        console.log(user);
-        if (user) return res.status(204).json({});
-        return error.createError(res, 404, 'User does not exist');
-    } catch (err) {
-        return error.handleError(err, res);
-    }
-};
-
-const getUserEvents = async (req, res) => {
-    try {
-        const userEvents = await Event.find({ owner: req.params.id });
-        if (userEvents) return res.status(200).json(userEvents);
-        return error.createError(res, 404, 'User does not exist');
-    } catch (err) {
-        return error.handleError(err, res)
-    }
-};
-
-const getUserObservations = async (req, res) => {
-    try {
-        const observations = await Observation.find({ owner: req.params.id });
-        if (observations) return res.status(200).json(observations);
-        return error.createError(res, 404, 'User does not exist');
-    } catch (err) {
-        return error.handleError(err, res);
-    }
-};
-
-const joinEvent = async (req, res) => {
-    try {
-        const user = await User.findById(req.params.id);
-        if (!user) return createError(res, 404, 'User does not exist');
-
-        const event = await Event.findById(req.params.eventId, '_id');
-        if (!event) return createError(res, 404, 'Event does not exist');
-        if (user.events.includes(event._id)) {
-            return error.createError(res, 422, 'User already joined the event');
-        }
-
-        user.events.push(event._id);
-        const updatedUser = await user.save();
-        return res.status(201).json(updatedUser);
-    } catch (err) {
-        return error.handleError(err, res);
-    }
-};
+    user.events.push(event._id);
+    const updatedUser = await user.save();
+    return res.status(201).json(updatedUser);
+});
 
 module.exports = {
     getUsers: getUsers,
