eaf2e089b3a95626282c814fe013398ab87e575d
kjuvi <quentin.vaucher@protonmail.com>
2020-04-20 15:40:51 +0200

    add a batch insertion script from csv files

---
+++ bootstrap-modos/README.md
@@ -0,0 +1,75 @@
+# Bootstrap-modos
+
+Bootstrap-modos is a CLI to insert multiple user into the database and link
+them to an event. The usage is very limited but should hopefully be enough
+for a "batch insertion" from a csv file.
+
+## Requirements
+
+- Python 3.7 or greater (it uses dataclass and f string)
+- [Requests](https://requests.readthedocs.io/en/master/)
+
+## Preparing the data
+
+The script reads data from two csv files, which should be in a `data/` folder,
+next to `bootstrap.py`:
+
+```
+./
+bootstrap.py
+data/
+    users_profiles.csv
+    event.csv
+```
+
+### users_profiles.csv
+
+This file contains the user and their optional profile. The header is the
+following:
+
+| pseudonym | email | password | age | gender | helper | helperFrequency | mobility |
+|-----------|-------|----------|-----|--------|--------|-----------------|----------|
+
+the required fields are:
+
+- pseudonym
+- email
+- password
+
+If a profile is defined, the following fields are also required:
+
+- age
+- gender
+- mobility
+
+Finally, note that defining a `helper` fields requires to define a
+`helperFrequency` field too.
+
+Here are the 3 possible cases that can be defined:
+
+- pseudonym,email,password
+- pseudonym,email,password,age,gender,,,mobility
+- pseudonym,email,password,age,gender,helper,helperFrequency,mobility
+
+Note that some fields have specific requirements too:
+
+- password must be >= 8 characters
+- email must be a valid email string
+- gender is an enum: [ 'm', 'f' ]. It can be null (meaning "other" than 'm' or 'f')
+- helper is an enum: [ white cane, walker, wheelchair ]
+- helperFrequency is an enum: [ rarely, sometimes, always ]
+- mobility is an enum: [ minimal, reduced, good, perfect ]
+
+### event.csv
+
+This file must contain only one event. All the users defined in the previous
+file will automatically be set as participant to this event. Note that this file
+is not required (in which case, the users won't be affected to any event).
+
+The header is the following:
+
+
+| title | password | beginning | ending | objective | numberOfImages |
+|-------|----------|-----------|--------|-----------|----------------|
+
+All the fields are required, except for `password` which is optional.
---
+++ bootstrap-modos/bootstrap.py
@@ -0,0 +1,284 @@
+#!/usr/bin/python
+import argparse
+import dataclasses
+import json
+import re
+import sys
+from collections import OrderedDict
+from csv import DictReader
+from dataclasses import dataclass
+from datetime import date
+from enum import Enum
+from typing import Any, List
+from getpass import getpass
+
+import requests
+
+#################################################
+#  Global variables
+#################################################
+BASE_URL = 'https://modos.heig-vd.ch/api/v1'
+HEADERS = {'Content-Type': 'application/json'}
+EVENT_ID: str = None
+
+
+#################################################
+#  Enum
+#################################################
+class Gender(Enum):
+    """Gender Enum used by a Profile."""
+    MALE = 'm'
+    FEMALE = 'f'
+    OTHER = None
+
+
+class Helper(Enum):
+    """Helper Enum used by a Profile."""
+    WHITE_CANE = 'white cane'
+    WALKER = 'walker'
+    WHEELCHAIR = 'wheelchair'
+
+
+class HelperFrequency(Enum):
+    """elperFrequency Enum used by a Profile."""
+    RARELY = 'rarely'
+    SOMETIMES = 'sometimes'
+    ALWAYS = 'always'
+
+
+class Mobility(Enum):
+    """Mobility Enum used by a Profile."""
+    MINIMAL = 'minimal'
+    REDUCED = 'reduced'
+    GOOD = 'good'
+    PERFECT = 'perfect'
+
+
+#################################################
+#  Models
+#################################################
+class JSONDataclass():
+    """A JSON serializable dataclass."""
+
+    def to_json(self):
+        """Return the json represntation of the class."""
+        return json.dumps(dataclasses.asdict(self))
+
+
+@dataclass
+class Credentials(JSONDataclass):
+    """Credentials data to authentify a user."""
+    email: str
+    password: str
+
+    def authentify(self):
+        """
+        Authentify with the setted credentials and update `HEADER` by adding
+        an Authorization header.
+        """
+        try:
+            r = requests.post(BASE_URL + '/authenticate',
+                              data=self.to_json(), headers=HEADERS)
+            if r.status_code != 200:
+                raise AssertionError(f' authentication error: {r.json()}')
+
+            token = r.json()['token']
+            HEADERS['Authorization'] = f'Bearer {token}'
+        except AssertionError as bad_status_code:
+            sys.exit(bad_status_code)
+
+
+@dataclass
+class Profile(JSONDataclass):
+    """Profile dataclass."""
+    age: int
+    gender: Gender
+    helper: Helper
+    helperFrequency: HelperFrequency
+    mobility: Mobility
+
+    def __init__(self, csv: OrderedDict):
+        """Populate the Profile dataclass from a csv row."""
+        self.age = csv.get('age')
+        self.gender = csv.get('gender')
+        self.helper = csv.get('helper')
+        self.helperFrequency = csv.get('helperFrequency')
+        self.mobility = csv.get('mobility')
+
+    def is_null(self) -> bool:
+        """Check if the profile is set or not."""
+        return (self.age or self.gender or self.helper or self.helperFrequency or self.mobility)
+
+    def to_json(self):
+        """Return the json represntation of the Profile dataclass."""
+        payload_dict = dataclasses.asdict(self)
+        if not self.helper:
+            payload_dict.pop('helper', None)
+            payload_dict.pop('helperFrequency', None)
+
+        return json.dumps(payload_dict)
+
+
+@dataclass
+class User(JSONDataclass):
+    """User dataclass."""
+    pseudonym: str
+    email: str
+    password: str
+
+    def __init__(self, csv: OrderedDict):
+        """Populate the User dataclass from a csv row."""
+        self.pseudonym = csv['pseudonym']
+        self.email = csv['email']
+        self.password = csv['password']
+        if not self._is_email_valid():
+            raise AssertionError(
+                f'AssertionError: Email is not valid ({self.email})')
+
+    def _is_email_valid(self) -> bool:
+        """Check if `email` is valid."""
+        regex = '^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$'
+        return True if re.search(regex, self.email) else False
+
+    def insert(self):
+        """Insert the user into the database."""
+        try:
+            print(f'inserting {self.pseudonym}...', end='')
+            r = requests.post(BASE_URL + '/users',
+                              data=self.to_json(), headers=HEADERS)
+            if r.status_code != 201:
+                raise AssertionError(f'insertion error: {r.json()}')
+
+            if EVENT_ID is not None:
+                r = requests.post(BASE_URL + '/users/join/' +
+                                  EVENT_ID, headers=HEADERS)
+                if r.status_code != 200:
+                    raise AssertionError(
+                        f'error while joining event: {r.json()}')
+            print(' ok')
+        except AssertionError as bad_status_code:
+            sys.exit(bad_status_code)
+
+    def delete(self):
+        """Delete the user from the database."""
+        try:
+            print(f'deleting {self.pseudonym}...', end='')
+            Credentials(self.email, self.password).authentify()
+
+            # delete the user
+            r = requests.delete(BASE_URL + '/users',
+                                headers=HEADERS)
+            if r.status_code != 204:
+                raise AssertionError(f' deletion error: {r.json()}')
+            print(' ok')
+
+        except AssertionError as bad_status_code:
+            sys.exit(bad_status_code)
+
+
+@dataclass
+class UserProfile(JSONDataclass):
+    """User and their Profile."""
+    user: User
+    profile: Profile
+
+    def __init__(self, csv_row: OrderedDict):
+        """Populate the UserProfile dataclass from a csv row."""
+        self.user = User(csv_row)
+        self.profile = Profile(csv_row)
+
+    def insert_profile(self):
+        """Insert the user's profile into the database."""
+        if self.profile.is_null() is None:
+            print(f'skip profile insertion for {self.user.pseudonym} (null)')
+            return
+        try:
+            print(f'inserting profile for {self.user.pseudonym}...', end='')
+            Credentials(self.user.email, self.user.password).authentify()
+            r = requests.post(BASE_URL + '/users/profile', headers=HEADERS,
+                              data=self.profile.to_json())
+
+            if r.status_code != 201:
+                raise AssertionError(f'insertion error: {r.json()}')
+            print(' ok')
+        except AssertionError as bad_status_code:
+            sys.exit(bad_status_code)
+
+    def insert_user_and_profile(self):
+        """Insert the user and his profile into the database."""
+        self.user.insert()
+        self.insert_profile()
+
+
+@dataclass
+class Event(JSONDataclass):
+    """Event dataclass."""
+    title: str
+    password: str
+    beginning: date
+    ending: date
+    objective: str
+    numberOfImages: int
+
+    def __init__(self, csv: OrderedDict):
+        self.title = csv['title']
+        self.password = csv['password']
+        self.beginning = csv['beginning']
+        self.ending = csv['ending']
+        self.objective = csv['objective']
+        self.numberOfImages = csv['numberOfImages']
+
+    def insert(self, credentials: Credentials):
+        """Insert the event into the database."""
+        try:
+            print(f'inserting event {self.title}...', end='')
+            credentials.authentify()
+
+            r = requests.post(BASE_URL + '/events',
+                              data=self.to_json(), headers=HEADERS)
+            if r.status_code != 201:
+                raise AssertionError(f'insertion error: {r.json()}')
+            EVENT_ID = r.json()['_id']
+            print(' ok')
+        except AssertionError as bad_status_code:
+            sys.exit(bad_status_code)
+
+
+#################################################
+#  CSV loaders
+#################################################
+def load_users_profiles(csv_path: str) -> List[UserProfile]:
+    """Load the csv containing the users and, optionally, their profile."""
+    users_profiles_csv = DictReader(open(csv_path))
+    try:
+        return [UserProfile(row) for row in users_profiles_csv]
+    except AssertionError as e:
+        sys.exit(e)
+
+
+def load_event(csv_path: str) -> Event:
+    """Load the csv containing the events."""
+    events_csv = DictReader(open(csv_path))
+    try:
+        return Event(list(events_csv)[0])
+    except IndexError:
+        raise FileNotFoundError  # no event for this bootstraping session...
+    except AssertionError as e:
+        sys.exit(e)
+
+
+if __name__ == "__main__":
+    try:
+        event: Event = load_event('./data/event.csv')
+        email = input('Enter your email: ')
+        password = getpass('Enter your password: ')
+        credentials = Credentials(email, password)
+        event.insert(credentials)
+    except FileNotFoundError:
+        pass  # no event for this bootstraping session...
+
+    users_profiles: List[UserProfile] = load_users_profiles(
+        './data/users_profiles.csv')
+
+    [up.insert_user_and_profile() for up in users_profiles]
+    # [up.user.delete() for up in users_profiles]
