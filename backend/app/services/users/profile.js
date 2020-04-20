749125b7975c406c961ad83eee3e06622422f56c
kjuvi <quentin.vaucher@protonmail.com>
2020-04-20 10:46:45 +0200

    fix json responses in user profile

--- backend/app/services/users/profile.js
+++ backend/app/services/users/profile.js
@@ -5,34 +5,36 @@ const { createAsyncRoute } = require('../utils');
 
 const getProfile = createAsyncRoute(async (req, res) => {
     const user = await User.findById(req.body.userId);
-    if (!user) return error.sendErrorrror(res, 404, 'User does not exist');
-    if (!user.profile) return error.sendErrorrror(res, 404, 'Profile does not exist');
-    return res.status(200).json(user.profile.toResponseJSON());
+    if (!user) return error.sendError(res, 404, 'User does not exist');
+    if (!user.profile) return error.sendError(res, 404, 'Profile does not exist');
+    return res.status(200).json(user.profile.toObject());
 });
 
 const createProfile = createAsyncRoute(async (req, res) => {
     const userId = req.body.userId;
     const user = await User.findById(userId);
 
-    if (!user) return error.sendErrorrror(res, 404, 'User does not exist');
+    if (!user) return error.sendError(res, 404, 'User does not exist');
+
+    console.log(req.body);
 
     user.profile = req.body;
     const updatedUser = await user.save();
 
-    return res.status(201).json(updatedUser.profile.toResponseJSON());
+    return res.status(201).json(updatedUser.profile.toObject());
 });
 
 const updateProfile = createAsyncRoute(async (req, res) => {
     const userId = req.body.userId;
     const user = await User.findById(userId);
 
-    if (!user) return error.sendErrorrror(res, 404, 'User does not exist');
-    if (!user.profile) return error.sendErrorrror(res, 404, 'Profile does not exist');
+    if (!user) return error.sendError(res, 404, 'User does not exist');
+    if (!user.profile) return error.sendError(res, 404, 'Profile does not exist');
 
     Object.assign(user.profile, req.body);
     const updatedUser = await user.save();
 
-    return res.status(200).json(updatedUser.profile.toResponseJSON());
+    return res.status(200).json(updatedUser.profile.toObject());
 });
 
 
