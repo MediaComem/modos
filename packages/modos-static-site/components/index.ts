e38530e61be9888617777c327e24196de3459ed6
Arthur Verdon <verdon.arthur@gmail.com>
2020-05-18 16:50:07 +0200

    remove useless code

--- packages/modos-static-site/components/index.ts
+++ packages/modos-static-site/components/index.ts
@@ -2,6 +2,5 @@ export * from './layout/Header';
 export * from './layout/Footer';
 export * from './layout/Layout';
 export * from './general-ui/Leaflet';
-export * from './general-ui/Section';
 export * from './general-ui/LanguageSelect';
 
