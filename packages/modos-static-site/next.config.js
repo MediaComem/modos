487f4aff9320ee28f8664fec9126f9224c85c494
Arthur Verdon <verdon.arthur@gmail.com>
2020-06-11 16:35:20 +0200

    fix min height for map in home

--- packages/modos-static-site/next.config.js
+++ packages/modos-static-site/next.config.js
@@ -3,7 +3,7 @@ const withPlugins = require('next-compose-plugins');
 // https://github.com/cyrilwanner/next-optimized-images
 const optimizedImages = require('next-optimized-images');
 
-const ASSET_PREFIX = process.env.ASSET_PREFIX;
+const ASSET_PREFIX = '_staging'//process.env.ASSET_PREFIX;
 
 module.exports = withPlugins(
   [
@@ -30,6 +30,6 @@ module.exports = withPlugins(
     // List of Next options: https://github.com/vercel/next.js/blob/canary/packages/next/next-server/server/config.ts#L12-L63
 
     assetPrefix: ASSET_PREFIX ? ASSET_PREFIX : '',
-    basePath: ASSET_PREFIX ? ASSET_PREFIX : ''
+    // basePath: ASSET_PREFIX ? ASSET_PREFIX : '' Experimental for now https://github.com/vercel/next.js/pull/9872
   }
 );
