e371d43a65d3ec865de241b616ce3af092b4c42e
Simon Oulevay <simon.oulevay@gmail.com>
2020-07-08 14:41:45 +0200

    Replace 3-space indentation by 2-space indention

--- packages/backend/ormconfig.ts
+++ packages/backend/ormconfig.ts
@@ -3,24 +3,24 @@ import { ConnectionOptions } from 'typeorm';
 import * as config from './src/config/config';
 
 const connectionOptions: ConnectionOptions = {
-   type: "postgres",
-   url: config.databaseUrl,
-   synchronize: false,
-   logging: false,
-   entities: [
-      "src/entity/**/*.ts"
-   ],
-   migrations: [
-      "src/migration/**/*.ts"
-   ],
-   subscribers: [
-      "src/subscriber/**/*.ts"
-   ],
-   cli: {
-      entitiesDir: "src/entity",
-      migrationsDir: "src/migration",
-      subscribersDir: "src/subscriber"
-   }
+  type: "postgres",
+  url: config.databaseUrl,
+  synchronize: false,
+  logging: false,
+  entities: [
+    "src/entity/**/*.ts"
+  ],
+  migrations: [
+    "src/migration/**/*.ts"
+  ],
+  subscribers: [
+    "src/subscriber/**/*.ts"
+  ],
+  cli: {
+    entitiesDir: "src/entity",
+    migrationsDir: "src/migration",
+    subscribersDir: "src/subscriber"
+  }
 }
 
 export = connectionOptions;
