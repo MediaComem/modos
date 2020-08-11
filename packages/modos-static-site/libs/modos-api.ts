2d005414d96d74a33f3812316f636e0a6ea22ede
Arthur Verdon <verdon.arthur@gmail.com>
2020-08-11 16:45:45 +0200

    Map can now fetch data from api

--- packages/modos-static-site/libs/modos-api.ts
+++ packages/modos-static-site/libs/modos-api.ts
@@ -1,16 +1,33 @@
 import { customFetch } from './fetch';
 
-const API_URL = process.env.API_URL && '';
-const API_TOKEN = process.env.API_TOKEN && '';
+// MODOS API CONFIG
+const API_URL =
+  process.env.NODE_ENV === 'production' ?
+    'https://modos.heig-vd.ch/api/v1' :
+    'http://localhost:3000/api/v1';
 
+const API_TOKEN =
+  process.env.NODE_ENV === 'production' ?
+    '' :
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOjEsImlhdCI6MTU5NzE1NDQyMSwiZXhwIjoxNTk3MTU4MDIxfQ.qsZgo5od7vZAEHIFnbyCn3Crem2_eqbt_XkX2DVkUpI';
 
+/**
+ *
+ * @returns All the observations of the modos api
+ */
 export const getObservations = async () => {
-
   try {
-    return await customFetch('/example-observations.json');
+    // uncomment this part to test with the example asset
+    // return await customFetch(`/example-observations.json`);
+
+    return await customFetch(
+      new Request(`${API_URL}/observations`, {
+        method: 'GET',
+        headers: new Headers({ Authorization: `Bearer ${API_TOKEN}` })
+      })
+    );
   } catch (err) {
     console.error(err);
     throw new Error('can not load observations');
   }
-
 };
