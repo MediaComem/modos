55a79f40385fcedd317409f449cc133781e4483a
nicolas.heigvd <nicolas.blanc1@heig-vd.ch>
2020-10-28 15:17:28 +0100

    Remove schema definition in queries

--- packages/backend/src/migration/1603889960061-initObservations.ts
+++ packages/backend/src/migration/1603889960061-initObservations.ts
@@ -3,7 +3,7 @@ import { MigrationInterface, QueryRunner } from "typeorm";
 export class initObservations1603889960061 implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`CREATE TABLE IF NOT EXISTS public.observations (id INTEGER GENERATED ALWAYS AS IDENTITY, user_id INT REFERENCES public.users(id), event_id INT REFERENCES public.events(id), has_image bool NOT NULL DEFAULT FALSE, has_description bool NOT NULL DEFAULT FALSE, description_id INT REFERENCES public.descriptions(id), latitude FLOAT NOT NULL DEFAULT 0.0, longitude FLOAT NOT NULL DEFAULT 0.0, position geometry(Point, 4326) NOT NULL, in_range BOOLEAN NOT NULL DEFAULT FALSE, eid INT REFERENCES public.edges(id), edist REAL NOT NULL, snap_geom geometry(Point, 4326) NOT NULL, created_at TIMESTAMP WITH TIME ZONE DEFAULT NOW(), updated_at TIMESTAMP WITH TIME ZONE DEFAULT NOW(), CONSTRAINT observations_pkey PRIMARY KEY (id));`, undefined);
+        await queryRunner.query(`CREATE TABLE IF NOT EXISTS observations (id INTEGER GENERATED ALWAYS AS IDENTITY, user_id INT REFERENCES users(id), event_id INT REFERENCES events(id), has_image bool NOT NULL DEFAULT FALSE, has_description bool NOT NULL DEFAULT FALSE, description_id INT REFERENCES descriptions(id), latitude FLOAT NOT NULL DEFAULT 0.0, longitude FLOAT NOT NULL DEFAULT 0.0, position geometry(Point, 4326) NOT NULL, in_range BOOLEAN NOT NULL DEFAULT FALSE, eid INT REFERENCES edges(id), edist REAL NOT NULL, snap_geom geometry(Point, 4326) NOT NULL, created_at TIMESTAMP WITH TIME ZONE DEFAULT NOW(), updated_at TIMESTAMP WITH TIME ZONE DEFAULT NOW(), CONSTRAINT observations_pkey PRIMARY KEY (id));`, undefined);
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
