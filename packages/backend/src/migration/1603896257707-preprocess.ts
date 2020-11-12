cdab73338f04a06d429f35eeccb557e2c336fcc6
nicolas.heigvd <nicolas.blanc1@heig-vd.ch>
2020-11-12 10:40:26 +0100

    Format SQL + minor fixes

--- packages/backend/src/migration/1603896257707-preprocess.ts
+++ packages/backend/src/migration/1603896257707-preprocess.ts
@@ -4,30 +4,67 @@ export class preprocess1603896257707 implements MigrationInterface {
     name = 'preprocess1603896257707'
 
     public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`CREATE FUNCTION "modos"."convert_to_integer"(v_input text) RETURNS INTEGER AS $$ DECLARE v_int_value INTEGER DEFAULT NULL;
+        await queryRunner.query(`
+            CREATE OR REPLACE FUNCTION "modos"."convert_to_integer"(v_input text)
+            RETURNS INTEGER AS
+            $$
+            DECLARE v_int_value INTEGER DEFAULT NULL;
             BEGIN
-                BEGIN v_int_value := v_input::INTEGER;
-                    EXCEPTION WHEN OTHERS THEN RETURN NULL;
+                BEGIN
+                    v_int_value := v_input::INTEGER;
+                    EXCEPTION WHEN OTHERS THEN
+                    --RAISE NOTICE 'Invalid integer value: "%".  Returning NULL.', v_input;
+                    RETURN NULL;
                 END;
                 RETURN v_int_value;
             END;
-        $$ LANGUAGE plpgsql;`);
+            $$
+            LANGUAGE plpgsql;
+        `, undefined);
 
-        await queryRunner.query(`CREATE FUNCTION "modos"."convert_to_float"(v_input text) RETURNS FLOAT AS $$ DECLARE v_int_value FLOAT DEFAULT NULL;
+        await queryRunner.query(`
+            CREATE OR REPLACE FUNCTION "modos"."convert_to_float"(v_input text)
+            RETURNS FLOAT AS
+            $$
+            DECLARE v_int_value FLOAT DEFAULT NULL;
             BEGIN
-                BEGIN v_int_value := v_input::FLOAT;
-                    EXCEPTION WHEN OTHERS THEN RETURN NULL;
+                BEGIN
+                    v_int_value := v_input::FLOAT;
+                    EXCEPTION WHEN OTHERS THEN
+                    --RAISE NOTICE 'Invalid float value: "%".  Returning NULL.', v_input;
+                    RETURN NULL;
                 END;
                 RETURN v_int_value;
             END;
-        $$ LANGUAGE plpgsql;`, undefined);
+            $$
+            LANGUAGE plpgsql;
+        `, undefined);
 
         await queryRunner.query(`CREATE INDEX "modos_edges_geom_idx" ON "modos"."edges" USING GIST (geom);`, undefined);
         await queryRunner.query(`CREATE INDEX "modos_nodes_geom_idx" ON "modos"."nodes" USING GIST (geom);`, undefined);
 
-        await queryRunner.query(`CREATE MATERIALIZED VIEW "modos"."network_convex_hull_v" AS (
-            SELECT 1::INTEGER AS id, ST_Multi(ST_Transform(ST_Buffer(ST_Transform(ST_ConvexHull(ST_Collect(geom)), 2056), 100), 4326))::geometry(MultiPolygon, 4326) AS geom FROM "modos"."edges"
-        );`, undefined);
+        await queryRunner.query(`
+            CREATE MATERIALIZED VIEW "modos"."network_convex_hull_v" AS (
+                SELECT
+                1::INTEGER AS id,
+                ST_Multi(
+                    ST_Transform(
+                    ST_Buffer(
+                        ST_Transform(
+                        ST_ConvexHull(
+                            ST_Collect(geom)
+                        ),
+                        2056
+                        ),
+                        100
+                    ),
+                    4326
+                    )
+                )::geometry(MultiPolygon, 4326) AS geom
+                FROM
+                "modos"."edges"
+            );
+        `, undefined);
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
--- packages/backend/src/migration/1603969816824-initSnapping.ts
+++ packages/backend/src/migration/1603969816824-initSnapping.ts
@@ -4,11 +4,124 @@ export class initSnapping1603969816824 implements MigrationInterface {
     name = 'initSnapping1603969816824'
 
     public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`CREATE FUNCTION "modos"."snap_point"() RETURNS trigger AS $$ DECLARE geomx geometry(Point,4326) := NULL; BEGIN IF to_jsonb(NEW) ? 'geom' THEN geomx := NEW.geom; ELSIF to_jsonb(NEW) ? 'position' THEN geomx := NEW.position; END IF; IF (SELECT ST_Intersects(ST_Transform(geomx, 2056), ST_Transform(network_convex_hull_v.geom, 2056)) FROM "modos"."network_convex_hull_v") THEN SELECT edges.id AS modos_edge_id, (ST_Transform(edges.geom, 2056) <-> ST_Transform(geomx, 2056))::double precision AS dist_to_nearest_edge, ST_Transform(ST_ClosestPoint(ST_Transform(edges.geom, 2056), ST_Transform(geomx, 2056)), 4326)::geometry(Point, 4326) AS snap_geom FROM "modos"."edges" ORDER BY dist_to_nearest_edge LIMIT 1 INTO NEW.eid, NEW.edist, NEW.snap_geom; END IF; RETURN NEW; END $$ LANGUAGE 'plpgsql';`, undefined);
-        await queryRunner.query(`CREATE FUNCTION "modos"."snap_point_simple"() RETURNS trigger AS $$ DECLARE geomx geometry(Point,4326) := NULL; BEGIN IF to_jsonb(NEW) ? 'geom' THEN geomx := NEW.geom; ELSIF to_jsonb(NEW) ? 'position' THEN geomx := NEW.position; END IF; NEW.snap_geom := (SELECT sub.snap_geom FROM (SELECT edges.id AS modos_edge_id, ST_Distance(ST_Transform(edges.geom, 2056), ST_Transform(geomx, 2056))::double precision AS dist_to_nearest_edge, ST_Transform(ST_ClosestPoint(ST_Transform(edges.geom, 2056), ST_Transform(geomx, 2056)), 4326)::geometry(Point, 4326) AS snap_geom FROM "modos"."edges" ORDER BY dist_to_nearest_edge LIMIT 1) AS sub); RETURN NEW; END $$ LANGUAGE 'plpgsql';`, undefined);
+        await queryRunner.query(`
+            CREATE OR REPLACE FUNCTION "modos"."snap_point"()
+            RETURNS trigger AS
+            $$
+            DECLARE
+                geomx geometry(Point,4326) := NULL;
+            BEGIN
+                IF to_jsonb(NEW) ? 'geom' THEN
+                    geomx := NEW.geom;
+                ELSIF to_jsonb(NEW) ? 'position' THEN
+                    geomx := NEW.position;
+                END IF;
+                IF (SELECT ST_Intersects(
+                    ST_Transform(geomx, 2056),
+                    ST_Transform("modos"."network_convex_hull_v".geom, 2056)
+                ) FROM "modos"."network_convex_hull_v")
+                THEN
+                    SELECT
+                        "modos"."edges".id AS modos_edge_id,
+                        (
+                        ST_Transform("modos"."edges".geom, 2056)
+                        <->
+                        ST_Transform(geomx, 2056)
+                        )::double precision AS dist_to_nearest_edge,
+                        ST_Transform(
+                            ST_ClosestPoint(
+                            ST_Transform("modos"."edges".geom, 2056),
+                            ST_Transform(geomx, 2056)
+                        ),
+                        4326
+                    )::geometry(Point, 4326) AS snap_geom
+                    FROM
+                        "modos"."edges"
+                    ORDER BY dist_to_nearest_edge
+                    LIMIT 1
+                    INTO NEW.eid, NEW.edist, NEW.snap_geom;
+                END IF;
+                RETURN NEW;
+            END
+            $$
+        LANGUAGE 'plpgsql';
+        `, undefined);
+        await queryRunner.query(`
+            CREATE OR REPLACE FUNCTION "modos"."snap_point_simple"()
+            RETURNS trigger AS
+            $$
+            DECLARE
+                geomx geometry(Point,4326) := NULL;
+            BEGIN
+                IF to_jsonb(NEW) ? 'geom' THEN
+                    geomx := NEW.geom;
+                ELSIF to_jsonb(NEW) ? 'position' THEN
+                    geomx := NEW.position;
+                END IF;
+                NEW.snap_geom := (
+                SELECT sub.snap_geom
+                FROM
+                    (
+                        SELECT
+                            "modos"."edges".id AS modos_edge_id,
+                            ST_Distance(
+                            ST_Transform("modos"."edges".geom, 2056),
+                            ST_Transform(geomx, 2056)
+                            )::double precision AS dist_to_nearest_edge,
+                            ST_Transform(
+                                ST_ClosestPoint(
+                                ST_Transform("modos"."edges".geom, 2056),
+                                ST_Transform(geomx, 2056)
+                            ),
+                            4326
+                        )::geometry(Point, 4326) AS snap_geom
+                        FROM
+                            "modos"."edges"
+                        ORDER BY dist_to_nearest_edge
+                        LIMIT 1
+                    ) AS sub
+                );
+                RETURN NEW;
+            END
+            $$
+        LANGUAGE 'plpgsql';
+        `, undefined);
         await queryRunner.query(`CREATE TRIGGER "snap_point_biut" BEFORE INSERT OR UPDATE ON "modos"."observation" FOR EACH ROW EXECUTE PROCEDURE "modos"."snap_point"();`, undefined);
         await queryRunner.query(`CREATE TRIGGER "snap_point_biut" BEFORE INSERT OR UPDATE ON "modos"."images" FOR EACH ROW EXECUTE PROCEDURE "modos"."snap_point"();`, undefined);
-        await queryRunner.query(`CREATE FUNCTION "modos"."set_geom_from_latlon"() RETURNS trigger AS $$ DECLARE latcol TEXT := NULL; loncol TEXT := NULL; lat FLOAT := NULL; lon FLOAT := NULL; BEGIN latcol:= (SELECT column_name FROM information_schema.columns WHERE table_schema = TG_TABLE_SCHEMA AND table_name = TG_TABLE_NAME AND column_name LIKE '%latitude%'); loncol := (SELECT column_name FROM information_schema.columns WHERE table_schema = TG_TABLE_SCHEMA AND table_name = TG_TABLE_NAME AND column_name LIKE '%longitude%'); EXECUTE 'select $1.' || loncol USING NEW INTO lon; EXECUTE 'select $1.' || latcol USING NEW INTO lat; NEW.geom := ST_SetSRID(ST_MakePoint(lon,lat), 4326); RETURN NEW; END $$ LANGUAGE 'plpgsql';`, undefined);
+        await queryRunner.query(`
+            CREATE OR REPLACE FUNCTION "modos"."set_geom_from_latlon"()
+            RETURNS trigger AS
+            $$
+            DECLARE
+                latcol TEXT := NULL;
+                loncol TEXT := NULL;
+                lat FLOAT := NULL;
+                lon FLOAT := NULL;
+            BEGIN
+                latcol:= (
+                    SELECT column_name
+                      FROM information_schema.columns
+                     WHERE table_schema = TG_TABLE_SCHEMA
+                       AND table_name = TG_TABLE_NAME
+                       AND column_name LIKE '%latitude%'
+                );
+                loncol := (
+                    SELECT column_name
+                      FROM information_schema.columns
+                     WHERE table_schema = TG_TABLE_SCHEMA
+                       AND table_name = TG_TABLE_NAME
+                       AND column_name LIKE '%longitude%'
+                );
+                EXECUTE 'select $1.' || loncol USING NEW INTO lon;
+                EXECUTE 'select $1.' || latcol USING NEW INTO lat;
+                NEW.geom := ST_SetSRID(
+                    ST_MakePoint(lon,lat), 4326
+                );
+                RETURN NEW;
+            END
+            $$
+        LANGUAGE 'plpgsql';
+        `, undefined);
         await queryRunner.query(`CREATE TRIGGER "set_geom_from_latlon_biut" BEFORE INSERT OR UPDATE ON "modos"."observation" FOR EACH ROW EXECUTE PROCEDURE modos.set_geom_from_latlon();`, undefined);
         await queryRunner.query(`CREATE TRIGGER "set_geom_from_latlon_biut" BEFORE INSERT OR UPDATE ON "modos"."images" FOR EACH ROW EXECUTE PROCEDURE modos.set_geom_from_latlon();`, undefined);
     }
