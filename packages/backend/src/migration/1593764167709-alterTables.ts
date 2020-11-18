b88642ab5cbccbbae5f079c1cf85e1a4abc99237
nicolas.heigvd <nicolas.blanc1@heig-vd.ch>
2020-11-18 11:58:47 +0100

    Add created_at and updated_at fields to user, event, observation and route tables

--- packages/backend/src/migration/1593764167709-alterTables.ts
+++ packages/backend/src/migration/1593764167709-alterTables.ts
@@ -11,11 +11,16 @@ export class alterTables1593764167709 implements MigrationInterface {
         await queryRunner.query(`ALTER TABLE "modos"."observation" ADD COLUMN "position" geometry(Point, 4326);`, undefined);
         await queryRunner.query(`ALTER TABLE "modos"."observation" ADD COLUMN "in_range" BOOLEAN NOT NULL DEFAULT FALSE;`, undefined);
         await queryRunner.query(`ALTER TABLE "modos"."observation" ADD COLUMN "edist" REAL;`, undefined);
+        await queryRunner.query(`ALTER TABLE "modos"."observation" ADD COLUMN "eid" INTEGER;`, undefined);
+        await queryRunner.query(`ALTER TABLE "modos"."observation" ADD COLUMN "created_at" TIMESTAMP WITH TIME ZONE DEFAULT NOW();`, undefined);
+        await queryRunner.query(`ALTER TABLE "modos"."observation" ADD COLUMN "updated_at" TIMESTAMP WITH TIME ZONE DEFAULT NOW();`, undefined);
         await queryRunner.query(`ALTER TABLE "modos"."observation" ADD COLUMN "snap_geom" geometry(Point, 4326)`, undefined);
 
         await queryRunner.query(`ALTER TABLE "modos"."event" ALTER COLUMN "id" DROP DEFAULT;`, undefined);
         await queryRunner.query(`DROP SEQUENCE "modos"."event_id_seq";`, undefined);
         await queryRunner.query(`ALTER TABLE "modos"."event" ALTER COLUMN "id" ADD GENERATED ALWAYS AS IDENTITY;`, undefined);
+        await queryRunner.query(`ALTER TABLE "modos"."event" ADD COLUMN "created_at" TIMESTAMP WITH TIME ZONE DEFAULT NOW();`, undefined);
+        await queryRunner.query(`ALTER TABLE "modos"."event" ADD COLUMN "updated_at" TIMESTAMP WITH TIME ZONE DEFAULT NOW();`, undefined);
 
         await queryRunner.query(`ALTER TABLE "modos"."profile" ALTER COLUMN "id" DROP DEFAULT;`, undefined);
         await queryRunner.query(`DROP SEQUENCE "modos"."profile_id_seq";`, undefined);
@@ -24,6 +29,8 @@ export class alterTables1593764167709 implements MigrationInterface {
         await queryRunner.query(`ALTER TABLE "modos"."user" ALTER COLUMN "id" DROP DEFAULT;`, undefined);
         await queryRunner.query(`DROP SEQUENCE "modos"."user_id_seq";`, undefined);
         await queryRunner.query(`ALTER TABLE "modos"."user" ALTER COLUMN "id" ADD GENERATED ALWAYS AS IDENTITY;`, undefined);
+        await queryRunner.query(`ALTER TABLE "modos"."user" ADD COLUMN "created_at" TIMESTAMP WITH TIME ZONE DEFAULT NOW();`, undefined);
+        await queryRunner.query(`ALTER TABLE "modos"."user" ADD COLUMN "updated_at" TIMESTAMP WITH TIME ZONE DEFAULT NOW();`, undefined);
 
         await queryRunner.query(`ALTER TABLE "modos"."user_events_event" ADD COLUMN "id" INTEGER GENERATED ALWAYS AS IDENTITY;`, undefined);
     }
