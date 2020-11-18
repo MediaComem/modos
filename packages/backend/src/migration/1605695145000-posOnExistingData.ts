88d355e8c201fc0235f5ecac8fdf91c7bb844c3b
nicolas.heigvd <nicolas.blanc1@heig-vd.ch>
2020-11-18 13:36:04 +0100

    Update observations, set missing fields

--- packages/backend/src/migration/1605695145000-posOnExistingData.ts
+++ packages/backend/src/migration/1605695145000-posOnExistingData.ts
@@ -4,9 +4,8 @@ export class posOnExistingData1605695145000 implements MigrationInterface {
     name = 'posOnExistingData1605695145000'
 
     public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`
-        
-        `, undefined);
+        await queryRunner.query(`UPDATE "modos"."observation" SET "updated_at"=NOW();`,
+            undefined);
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
