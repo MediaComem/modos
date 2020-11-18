8a9fa7063c7d860881e6589d8ffa8c175fa704f1
nicolas.heigvd <nicolas.blanc1@heig-vd.ch>
2020-11-18 13:38:36 +0100

    Reset constraints

--- packages/backend/src/migration/1605695145000-posOnExistingData.ts
+++ packages/backend/src/migration/1605695145000-posOnExistingData.ts
@@ -4,8 +4,10 @@ export class posOnExistingData1605695145000 implements MigrationInterface {
     name = 'posOnExistingData1605695145000'
 
     public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`UPDATE "modos"."observation" SET "updated_at"=NOW();`,
-            undefined);
+        await queryRunner.query(`UPDATE "modos"."observation" SET "updated_at"=NOW();`, undefined);
+        await queryRunner.query(`ALTER TABLE "modos"."observation" ALTER COLUMN "position" SET NOT NULL;`, undefined);
+        await queryRunner.query(`ALTER TABLE "modos"."observation" ALTER COLUMN "edist" SET NOT NULL;`, undefined);
+        await queryRunner.query(`ALTER TABLE "modos"."observation" ALTER COLUMN "snap_geom" SET NOT NULL;`, undefined);
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
