66f8a358d248a2a42c3291b96686a6c4b03f71f0
Arthur Verdon <verdon.arthur@gmail.com>
2020-11-19 17:53:34 +0100

    complete carto integration

--- packages/modos-static-site/components/map-app/ModosFootpathLayer.tsx
+++ packages/modos-static-site/components/map-app/ModosFootpathLayer.tsx
@@ -1,11 +1,38 @@
-import React from 'react';
-import { TileLayer } from 'react-leaflet';
+import React, { useEffect } from 'react';
+import { useLeaflet } from 'react-leaflet';
+import carto from '@carto/carto.js';
 
-const URL =
-  'https://cartocdn-gusc-c.global.ssl.fastly.net/verdonarthur/api/v1/map/11715edd6df580a3e8f0b69b175012b4:1605615941765/1/{z}/{x}/{y}.png?api_key=default_public';
-const ModosFootpathLayer = () => (
-  <TileLayer minZoom={14} maxZoom={18} url={URL} />
-);
+const ModosFootpathLayer = () => {
+  const LeafletContext = useLeaflet();
+  let tileLayerOptions: any = {};
+  useEffect(() => {
+    const cartoClient = new carto.Client({
+      apiKey: 'default_public',
+      username: 'verdonarthur'
+    });
+    const modosWalknetSource = new carto.source.SQL(
+      'SELECT * FROM modos_walknet'
+    );
+    const modosWalknetStyle = new carto.style.CartoCSS(`
+      #layer {
+        line-color: #4CC8A3;
+        line-width: 1.5;
+        line-opacity: 1;
+      }
+    `);
+
+    const modosWalknetLayer = new carto.layer.Layer(
+      modosWalknetSource,
+      modosWalknetStyle
+    );
+
+    cartoClient.addLayer(modosWalknetLayer);
+    tileLayerOptions = cartoClient
+      .getLeafletLayer()
+      .addTo(LeafletContext.layerContainer);
+  }, []);
+  return <div />;
+};
 
 // Bellow is the code begining of the integration of VectorGrid raster from
 // a geojson. TODO: Check with OEZ for perf
