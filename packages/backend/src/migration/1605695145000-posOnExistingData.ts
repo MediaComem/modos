9ad91eb2acf596dd2e0e20e5604bd8e0f82f191c
nicolas.heigvd <nicolas.blanc1@heig-vd.ch>
2020-11-20 08:29:47 +0100

    Minor fix

--- packages/backend/src/migration/1605695145000-posOnExistingData.ts
+++ packages/backend/src/migration/1605695145000-posOnExistingData.ts
@@ -4,7 +4,7 @@ export class posOnExistingData1605695145000 implements MigrationInterface {
     name = 'posOnExistingData1605695145000'
 
     public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`UPDATE "modos"."observation" SET "updated_at"=NOW();`, undefined);
+        await queryRunner.query(`UPDATE "modos"."observation" SET "updatedAt"=NOW();`, undefined);
         await queryRunner.query(`ALTER TABLE "modos"."observation" ALTER COLUMN "position" SET NOT NULL;`, undefined);
         await queryRunner.query(`ALTER TABLE "modos"."observation" ALTER COLUMN "edist" SET NOT NULL;`, undefined);
         await queryRunner.query(`ALTER TABLE "modos"."observation" ALTER COLUMN "eid" SET NOT NULL;`, undefined);
