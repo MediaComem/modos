f2a531a16045bb1eb9782bea379417b5ccf6a686
Arthur Verdon <verdon.arthur@gmail.com>
2020-12-08 11:48:23 +0100

    add leaflet and validate position page

--- packages/mobile-app/src/app/contribution/set-global-position/set-global-position.page.ts
+++ packages/mobile-app/src/app/contribution/set-global-position/set-global-position.page.ts
@@ -1,6 +1,20 @@
 import { Component, OnInit } from '@angular/core';
 import { Capacitor, Plugins } from '@capacitor/core';
 import { Location } from '../../models/location.model';
+import { Map, tileLayer, marker } from 'leaflet';
+import { AlertController, LoadingController } from '@ionic/angular';
+
+// resolve bug with leaflet not loading the icon
+import 'leaflet/dist/images/marker-shadow.png';
+import 'leaflet/dist/images/marker-icon-2x.png';
+import { Router } from '@angular/router';
+import { NavParamsService } from 'src/app/services/nav-params.service';
+
+const DEFAULT_MAP_ZOOM = 16;
+
+// following position is ~Yverdon-Les-Bains
+const DEFAULT_LAT = 46.7785;
+const DEFAULT_LNG = 6.6412;
 
 @Component({
   selector: 'app-set-global-position',
@@ -8,14 +22,49 @@ import { Location } from '../../models/location.model';
   styleUrls: ['./set-global-position.page.scss'],
 })
 export class SetGlobalPositionPage implements OnInit {
-  private location = new Location();
+  public map: Map;
+  public markerPosition: any;
+  public location = new Location();
 
-  constructor() {}
+  constructor(
+    public loadingController: LoadingController,
+    private alertCtrl: AlertController,
+    private param: NavParamsService,
+    private router: Router
+  ) {}
 
   async ngOnInit() {}
 
   async ionViewWillEnter() {
-    await this.autoLocate();
+    // init map and base coordinate
+    this.location.lat = DEFAULT_LAT;
+    this.location.lng = DEFAULT_LNG;
+    this.loadLeafletMap();
+
+    await this.refreshMapPosition();
+  }
+
+  private showErrorLocalisationAlert() {
+    this.alertCtrl
+      .create({
+        header: `Impossible d'obtenir la localisation`,
+        message: `Sans localisation, il est impossible d'effectuer une observation.`,
+        buttons: [
+          {
+            text: `Retour à l'accueil`,
+            handler: () => this.router.navigate(['/home']),
+          },
+        ],
+        backdropDismiss: false,
+      })
+      .then((alertEl) => {
+        alertEl.present();
+        alertEl
+          .onDidDismiss()
+          .then(() => this.router.navigate(['/home']))
+          .catch((err) => {});
+      })
+      .catch((err) => {});
   }
 
   /**
@@ -23,7 +72,7 @@ export class SetGlobalPositionPage implements OnInit {
    */
   private async autoLocate() {
     if (!Capacitor.isPluginAvailable('Geolocation')) {
-      console.error('error GPS');
+      this.showErrorLocalisationAlert();
       return;
     }
 
@@ -37,5 +86,70 @@ export class SetGlobalPositionPage implements OnInit {
 
     this.location.lat = position.coords.latitude;
     this.location.lng = position.coords.longitude;
+  }
+
+  /**
+   * Initialize a Leaflet map in the div with ID 'map'
+   */
+  private loadLeafletMap() {
+    this.map = new Map('map').setView(
+      [this.location.lat, this.location.lng],
+      DEFAULT_MAP_ZOOM
+    );
+    tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution:
+        'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors,<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>',
+    }).addTo(this.map);
+  }
+
+  /**
+   * Refresh the position of the person on the map
+   */
+  private refreshMarkerPos() {
+    if (this.markerPosition) {
+      this.map.removeLayer(this.markerPosition);
+    }
+
+    this.markerPosition = marker([this.location.lat, this.location.lng]).addTo(
+      this.map
+    );
+  }
+
+  /**
+   * Refresh the map position and the marker of the person
+   */
+  private async refreshMapPosition() {
+    const loading = await this.loadingController.create({
+      message: 'Chargement des coordonnées GPS',
+    });
+    await loading.present();
+
+    try {
+      await this.autoLocate();
+      this.refreshMarkerPos();
+      this.map.setView(
+        [this.location.lat, this.location.lng],
+        DEFAULT_MAP_ZOOM
+      );
+    } catch (err) {
+      alert(err);
+    }
+
+    await loading.dismiss();
+  }
+
+  /**
+   *
+   */
+  public async getLocalisation() {
+    await this.refreshMapPosition();
+  }
+
+  /**
+   * goToSummary
+   */
+  public goToSummary() {
+    this.param.location = this.location;
+    this.router.navigate(['/contribution/obstacle-summary']);
   }
 }
--- packages/mobile-app/src/app/services/nav-params.service.ts
+++ packages/mobile-app/src/app/services/nav-params.service.ts
@@ -1,12 +1,14 @@
 import { Injectable } from '@angular/core';
 import { Image } from '../models/image.model';
+import { Location } from '../models/location.model';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class NavParamsService {
   private _obstacle: string;
   private _image: string;
+  private _location: Location;
 
   set obstacle(o: string) {
     this._obstacle = o;
@@ -20,5 +22,12 @@ export class NavParamsService {
   }
   get image(): string {
     return this._image;
+  }
+
+  set location(i: Location) {
+    this._location = i;
+  }
+  get location(): Location {
+    return this._location;
   }
 }
